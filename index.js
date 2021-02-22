716d80217802942fcdaa58e096458e93b7323157
Raihan Preena <raihanpreena@gmail.com>
2021-02-22 03:31:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,1 @@
-Console.error(Halp!)
-console.log(I am a logger!)
-console.log("I am a logger!")
-Console.log("I would be a logger")
-console.log("I would be a logger.")
+Console.log("I am a logger.")
